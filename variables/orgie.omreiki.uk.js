ecdf6fec225ee738ae326854c4f53f0fb48a4753
esculapeso <esculapeso@gmail.com>
2024-01-28 23:20:53 +0100

    rearrange files

---
+++ variables/orgie.omreiki.uk.js
@@ -0,0 +1,55 @@
+let focusText = "Orgies";
+
+let videosForFocus = [
+          { id: "5Lzi4T6mu0U", name: "Autogenic training EN" },
+          { id: "rCcFkKFB0Ak", name: "Autogenic training EN" },
+          { id: "t2wvQCPxOXM", name: "Autogenic training EN" },
+          { id: "E_sdaDwa2Ek", name: "Autogenic training EN Male" },
+          { id: "MFxlK1ZvOmA", name: "Autogenic training EN Male" },
+          { id: "SirqUpc0iKw", name: "Autogenic training EN" },
+          { id: "ImqheIyp3dA", name: "Autogenic training DE" },
+          { id: "-tlo6lPYTFw", name: "Autogenic training DE" },
+          { id: "zJYTL4Wi_to", name: "Autogenic training DE" },
+          { id: "Bt6FF2Z5XU4", name: "Autogenic training PL" },
+          { id: "gxxI_sxkKs0", name: "Autogenic training PL" },
+          { id: "Vr_qIxpA6A4", name: "Autogenic training PL" },
+          { id: "fdniUMRmpEM", name: "Autogenic training PL Male" },
+          { id: "u4pHFI73tlo", name: "Autogenic training PL Male" },
+          { id: "Qdluuj-hRNM", name: "Rush Destiny" },
+        ];
+
+let imagesForFocus =  [
+		{ caption: "Cleopatra", filepath: "https://orgie.omreiki.uk/wp-content/uploads/2024/01/cleo_warm.png" },
+		{ caption: "Red Cleopatra", filepath: "https://orgie.omreiki.uk/wp-content/uploads/2024/01/cleo_warm_red.png" },
+        ];
+
+
+let defaultSession = [
+	{"page": "orgie", "name":"Cleopatra", "Session time":"Sun, 28 Jan 2024 20:50:53 GMT","Focus Text":"Orgies","videoId":"Bt6FF2Z5XU4","imageData":"url(\"https://orgie.omreiki.uk/wp-content/uploads/2024/01/cleo_warm.png\")","ImageCaption":"Quantum","sideText":"ORGIE","people":[{"role":"therapist","data":"none"},{"role":"person1","data":"none"},{"role":"person2","data":"none"},{"role":"person3","data":"none"},{"role":"person4","data":"none"},{"role":"person5","data":"none"},{"role":"person6","data":"none"}],"qrngInterval":938,"isPyramid":false,"innerBgColorLeft":"","innerBgColorRight":""},
+]
+
+let emotionsList = [
+    { name: "Happy", value: 4 },
+    { name: "Joyful", value: 3 },
+    { name: "Proud", value: 4 },
+    { name: "Satisfied", value: 1 },
+  ]
+
+ let energiesList = [
+    { name: "Energy", value: 4 },
+    { name: "Power", value: 3 },
+    { name: "Strength", value: 4 },
+    { name: "Willpower", value: 1 },
+  ]
+
+ let healthList = [
+    { name: "Heart", value: 4, link: "https://en.wikipedia.org/wiki/Heart" },
+    { name: "Lungs", value: 3, link: "https://en.wikipedia.org/wiki/Lungs" },
+    { name: "Vagus", value: 4, link: "https://en.wikipedia.org/wiki/Vagus" },
+    { name: "Liver", value: 1, link: "https://en.wikipedia.org/wiki/Liver" },
+    { name: "Intestine", value: 1, link: "https://en.wikipedia.org/wiki/Gastrointestinal_tract" },
+    { name: "Lymph", value: 1, link: "https://en.wikipedia.org/wiki/Lymph" },
+    { name: "Blood", value: 1, link: "https://en.wikipedia.org/wiki/Blood" },
+    { name: "Memory", value: 1, link: "https://en.wikipedia.org/wiki/Memory" },
+    { name: "Neurons", value: 1, link: "https://en.wikipedia.org/wiki/Neuron" },
+  ]
